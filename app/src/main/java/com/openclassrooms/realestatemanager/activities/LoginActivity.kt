05dd92663e24b7d4969e4bd383d002966b95a3f4
Aceman74 <thedmx74@hotmail.fr>
2019-08-19 19:49:25 +0200

    Settings dimens for different size, layout for login.

--- app/src/main/java/com/openclassrooms/realestatemanager/activities/LoginActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/activities/LoginActivity.kt
@@ -1,27 +1,30 @@
-package com.openclassrooms.realestatemanager
+package com.openclassrooms.realestatemanager.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
+import com.openclassrooms.realestatemanager.R
 
 /**
  * Created by Lionel JOFFRAY - on 06/08/2019.
  */
 
-class MainActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity(){
    lateinit var textViewMain:TextView
     lateinit var textViewQuantity:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        textViewMain = activity_main_activity_text_view_main    //  Fix the ID from second activity to main activity.
+        setContentView(R.layout.activity_login)
+      /**  textViewMain = activity_main_activity_text_view_main    //  Fix the ID from second activity to main activity.
         textViewQuantity = activity_main_activity_text_view_quantity
         configureTextViewMain()
         configureTextViewQuantity()
+      */
     }
 
+    /**
+     *
     private fun configureTextViewMain() {
         textViewMain.textSize = 15f
         textViewMain.text = "Le premier bien immobilier enregistré vaut "
@@ -32,4 +35,6 @@ class MainActivity : AppCompatActivity(){
         textViewQuantity.textSize = 20f
         textViewQuantity.text = quantity.toString()   //  Fix the variable, convert from int to string.
     }
+
+     */
 }
